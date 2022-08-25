72ab91691474c40ddf4bff3922398015a15621de
José Gonçalves <joseinacio774@gmail.com>
2022-08-25 12:56:28 -0300

    interação com o dom

--- assets/js/firebase/helfire-club.js
+++ assets/js/firebase/helfire-club.js
@@ -2,7 +2,8 @@ import app from './app.js'
 import {
   getFirestore,
   collection,
-  addDoc
+  addDoc,
+  getDocs
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js'
 
 export async function subscriptionHellfireClub(subscription) {
@@ -12,4 +13,14 @@ export async function subscriptionHellfireClub(subscription) {
   const hellfireClubcollection = collection(db, 'hellfire-club')
   const docRef = await addDoc(hellfireClubcollection, subscription)
   return docRef.id
+}
+
+export async function getHellfiresubscription() {
+  const db = getFirestore(app)
+  const hellfireClubcollection = collection(db, 'hellfire-club')
+  const hellfireClubcollectionSnapshot = await getDocs(hellfireClubcollection)
+  const subscription = hellfireClubcollectionSnapshot.docs.map(doc =>
+    doc.data()
+  )
+  return subscription
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import { subscriptionHellfireClub } from './firebase/helfire-club.js'
+import { getHellfiresubscription } from './firebase/helfire-club.js'
 
 //var de cada camp do form
 const txtName = document.getElementById('txtName')
@@ -18,4 +19,18 @@ btnSubscribe.addEventListener('click', async () => {
   //salvar as infor
   const subscriptionId = await subscriptionHellfireClub(subscription)
   console.log(`Escrito com Sucesso${subscriptionId}`)
+  //limpa os campos assim que enviar para o firebasee
+  name.value = ''
+  email.value = ''
+  level.value = ''
+  character.value = ''
+
+  window.alert(`olá ${txtName} Sua Inscrição fi feita com sucesso!`)
 })
+
+//quando a tela carregarvai mostras as infomações la do banco de dados
+async function loadDate() {
+  const subscriptions = await getHellfiresubscription()
+  console.log(subscriptions)
+}
+loadDate()
